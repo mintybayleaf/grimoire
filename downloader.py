7190ca9ef16322ab3d1fd6b7dedecefb98545445
Bailey Kocin <bailey.kocin@gmail.com>
2026-09-13 21:21:36 -0400

    stream cards with downloader

--- downloader.py
+++ downloader.py
@@ -1,5 +1,8 @@
 import dataclasses
 import enum
+import gzip
+import json
+from collections.abc import Generator
 from datetime import datetime
 from pathlib import Path
 from typing import BinaryIO, Final
@@ -60,6 +63,23 @@ class BulkDownload:
     compressed_size: int
 
 
+def get_download_path_for_type(download_type: BulkDownloadType):
+    home_path = Path.home()
+    config_path = home_path / ".grimoire"
+    download_path = config_path / "downloads" / download_type.value
+    return download_path
+
+
+def to_file_timestamp(timestamp: str) -> int:
+    return int(datetime.fromisoformat(timestamp).timestamp())
+
+
+def timestamp_from_file(file: Path) -> int:
+    file_name = file.name
+    index = file.name.index(".")
+    return int(file_name[:index])
+
+
 def download_bulk_file(download_type: BulkDownloadType):
     downloads: list[BulkDownload] = [
         BulkDownload(**download) for download in fetch_json(BULK_DATA)
@@ -68,13 +88,35 @@ def download_bulk_file(download_type: BulkDownloadType):
     download = next(filter(lambda d: d.type == download_type.value, downloads))
 
     if download:
-        home_path = Path.home()
-        config_path = home_path / ".grimoire"
-        download_path = config_path / "downloads" / download_type.value
+        download_path = get_download_path_for_type(download_type)
         download_path.mkdir(parents=True, exist_ok=True)
-        formatted_updated_at = datetime.fromisoformat(download.updated_at).strftime(
-            "%Y%m%dT%H%M%S"
-        )
-        download_file = download_path / f"{formatted_updated_at}.jsonl.gz"
+        timestamp = to_file_timestamp(download.updated_at)
+        download_file = download_path / f"{timestamp}.jsonl.gz"
         with open(download_file, "wb", buffering=False) as file:
             download_json(download.jsonl_download_uri, file)
+
+
+@dataclasses.dataclass(frozen=True, slots=True)
+class MagicCard:
+    id: str
+    name: str
+
+
+def stream_latest_for_download_type(download_type: BulkDownloadType) -> Generator[str]:
+    download_path = get_download_path_for_type(download_type)
+    files = sorted(
+        (timestamp_from_file(file) for file in download_path.glob("*.jsonl.gz")),
+        reverse=True,
+    )
+    if files:
+        timestamp = files[0]
+        with gzip.open(
+            download_path / Path(f"{timestamp}.jsonl.gz"), "rt", encoding="utf-8"
+        ) as file:
+            yield from file
+
+
+def stream_latest_magic_cards(download_type: BulkDownloadType) -> Generator[MagicCard]:
+    for line in stream_latest_for_download_type(download_type):
+        data = json.loads(line.strip())
+        yield MagicCard(id=data["id"], name=data["name"])
